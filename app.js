982e37da2770203cdf83f239956e30793f762944
Adrian <adrianramon2003@gmail.com>
2023-06-01 02:34:08 +0200

    Más comentarios en el código

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Requerimos paquetes necesarios
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -7,29 +9,32 @@ require('dotenv').config()
 const flash =require('connect-flash')
 const session = require('express-session')
 const passport = require('passport')
-const { database_desarrollo } = require('./keys')
+const { database_desarrollo } = require('./keys') // Importa la configuración de la base de datos.
 const indexRouter = require('./routes/index');
-const authenticationRouter = require('./routes/authentication');
+const authenticationRouter = require('./routes/authentication'); // Importa las rutas para la autenticación.
 
-const mysqlStore = require('express-mysql-session')(session);
+const mysqlStore = require('express-mysql-session')(session);  // Crea un almacén de sesión utilizando MySQL.
 
 const app = express();
-require('./config/passport')
+require('./config/passport') // Importa la configuración de Passport para la autenticación.
 
-// view engine setup
+// Configuración del motor de vistas
 app.set('views', path.join(__dirname, 'views'));
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); 
 
 
+// Middlewares
 
+  // Creacion de sesion
 app.use(session({
   secret: 'tfgsession',
   resave: false,
   saveUninitialized: false,
-  store: new mysqlStore(database_desarrollo)
+  store: new mysqlStore(database_desarrollo) // Almacena las sesiones en la base de datos MySQL.
 }))
 
+// Middlewares
 app.use(flash())
 app.use(logger('dev'));
 app.use(express.json());
@@ -39,16 +44,16 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use((req,res,next)=>{
-  app.locals.success=req.flash('success')
-  app.locals.message=req.flash('message')
-  app.locals.user = req.user
+  app.locals.success=req.flash('success') // Variables locales para los mensajes flash exitosos.
+  app.locals.message=req.flash('message') // Variables locales para los mensajes flash.
+  app.locals.user = req.user // Variable local para el usuario logueado.
   next()
 })
 
 app.use('/', indexRouter);
 app.use('/', authenticationRouter);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // Configura la carpeta de archivos estáticos.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
