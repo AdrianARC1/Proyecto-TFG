cdd4899eff27b8926f5fa3b3f0e8a7ce8f5805fd
Adrian <adrianramon2003@gmail.com>
2023-06-02 00:55:04 +0200

    Añadidos más mixins en sass

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -5,19 +5,19 @@ const fs = require('fs'); // Para interactuar con archivos
 
 module.exports={
 
-    getIndex:async(req, res, next) => {
-        const [qrcustom] = await pool.query('SELECT * FROM codigosqr WHERE codigoqrURL IS NOT NULL AND (opcionBody IS NOT NULL AND opcionBody != "square") AND (opcionEye IS NOT NULL AND opcionEye != "frame0") AND (opcionEyeBall IS NOT NULL AND opcionEyeBall != "ball0") AND (bgColor NOT IN ("#f00000", "#ffffff")) AND bodyColor != "#000000";'); // Seleccionar codigos especificos
-        const [qrnormales] = await pool.query('SELECT * FROM codigosqr WHERE codigoqrURL IS NOT NULL AND opcionBody IS NULL OR opcionBody = "square" AND opcionEye IS NULL OR opcionEye = "frame0" AND opcionEyeBall IS NULL OR opcionEyeBall = "ball0" AND bgColor = "#f00000" or bgColor = "#ffffff" AND bodyColor = "#000000" LIMIT 3;');
-        const successMessage = req.flash('success');
+      getIndex:async(req, res, next) => {
+          const [qrcustom] = await pool.query('SELECT * FROM codigosqr WHERE codigoqrURL IS NOT NULL AND (opcionBody IS NOT NULL AND opcionBody != "square") AND (opcionEye IS NOT NULL AND opcionEye != "frame0") AND (opcionEyeBall IS NOT NULL AND opcionEyeBall != "ball0") AND (bgColor NOT IN ("#f00000", "#ffffff")) AND bodyColor != "#000000";'); // Seleccionar codigos especificos
+          const [qrnormales] = await pool.query('SELECT * FROM codigosqr WHERE codigoqrURL IS NOT NULL AND opcionBody IS NULL OR opcionBody = "square" AND opcionEye IS NULL OR opcionEye = "frame0" AND opcionEyeBall IS NULL OR opcionEyeBall = "ball0" AND bgColor = "#f00000" or bgColor = "#ffffff" AND bodyColor = "#000000" LIMIT 3;');
+          const successMessage = req.flash('success');
 
-        res.render('index', {qrcustom, qrnormales, user: req.user, success: successMessage});
-    },
+          res.render('index', {qrcustom, qrnormales, user: req.user, success: successMessage});
+      },
 
-    getQrCustom:function(req, res, next) {
-        res.render('qrcustom');
-    },
+      getQrCustom:function(req, res, next) {
+          res.render('qrcustom');
+      },
 
-    getBiblioteca:async(req, res, next)=> {
+      getBiblioteca:async(req, res, next)=> {
         const [resul] = await pool.query('SELECT * FROM codigosqr WHERE codigoqrURL IS NOT NULL;'); // Seleccionar todos los codigos qr de la tabla que no tengan el campo codigoqrURL null
       
         res.render('biblioteca', {resul});
